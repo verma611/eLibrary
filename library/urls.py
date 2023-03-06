bf69c45f7e6d75ed63eb7ded318eaa47531d7f5c
verma611 <63016115+verma611@users.noreply.github.com>
2023-03-06 14:58:09 -0800

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('books.urls')),
-]
-
-if settings.DEBUG: 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
